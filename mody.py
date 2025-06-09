7e983428cbd30a99c1c055021eac6b883c175b6e
Khhhgh <103472630+Khhhgh@users.noreply.github.com>
2025-06-09 15:13:35 +0300

    Update mody.py

--- mody.py
+++ mody.py
@@ -2,12 +2,12 @@ import os
 
 
 class Mody(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7610698647:AAGc0vKL1iYTchSGJowB7FzlLWhwrPTg3V4")
 
-    APP_ID = int(os.environ.get("APP_ID", 25281175))
+    APP_ID = int(os.environ.get("APP_ID", 22654154))
 
-    API_HASH = os.environ.get("API_HASH", "6d99cb2b60a2c519fc1f99bd19565730")
+    API_HASH = os.environ.get("API_HASH", "cf260fa60f76561cb6bf47a12241669b")
     
-    BOT_USER = os.environ.get("BOT_USER", "")
+    BOT_USER = os.environ.get("BOT_USER", "Hgfghtddfvbot")
     
-    SESSION = os.environ.get("SESSION", "")
+    SESSION = os.environ.get("SESSION", "AgFZrMoAMWPSH82gV3HpUuoAgjwAFoEodrYCnQt7_Wjpl2FHP11XAvBY4mo4yi_fwrHhAU4eXCfee4-mCvBkWCKR4t5p5duxXmWcjZUaWMqZ9azD3rruOr8BhZJvgT2RFARDb3Gcr2jBRHTX3zQ6FznwwBYFroQp3xohaPFxp_0U-aXGOyza7-LCApbumc3U1h_gkmV1fosZszE5jPcw_Wdu6jrv_I7WN9PnkTz-dxJjkSJhrv6vJKslRN93zzgjWBzobYB3-QzXjffKAxLIRIGuJCr91znlx5wIl2wYGZ7npVlGvL-79ZWqdIpOvH_IPEZtT8nIIIiNu4-T4vGitvzmr6aPlwAAAAGrkCmxAA")
